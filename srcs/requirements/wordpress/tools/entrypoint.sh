4b5c0bc8e8f4a32414081ded142bc78c1a69aad0
BuildTools <unconfigured@null.spigotmc.org>
2023-05-02 17:20:29 +0000

    Wordpress working on first install

--- srcs/requirements/wordpress/tools/entrypoint.sh
+++ srcs/requirements/wordpress/tools/entrypoint.sh
@@ -1,4 +1,4 @@
 wp core download
 wp config create --dbhost=mariadb --dbprefix=wp_  --dbname=wordpress --dbuser=wordpress --dbpass=1234
-wp core install --title="Your Blog Title" --admin_name=shai --admin_password=1234 --admin_email=you@example.com
+wp core install --url=https://92.107.7.129:1234 --title="Your Blog Title" --admin_name=shai --admin_password=1234 --admin_email=you@example.com
 php-fpm7.4 -F
