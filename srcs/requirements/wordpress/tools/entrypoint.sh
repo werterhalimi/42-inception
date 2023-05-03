9efaa6a128fc2b322d0ec64376efa2c63d9a7ebe
BuildTools <unconfigured@null.spigotmc.org>
2023-05-03 16:49:44 +0000

    now usiing bullseye version of debian and using env variables

--- srcs/requirements/wordpress/tools/entrypoint.sh
+++ srcs/requirements/wordpress/tools/entrypoint.sh
@@ -1,7 +1,8 @@
 if ! ([ -f "/var/www/wp-config.php" ];) then
 	wp core download
-	wp config create --dbhost=mariadb --dbprefix=wp_  --dbname=wordpress --dbuser=wordpress --dbpass=1234
-	wp core install --url=https://92.107.7.129:1234 --title="Your Blog Title" --admin_name=shai --admin_password=1234 --admin_email=you@example.com
+	wp config create --dbhost=$DB_HOST --dbprefix=$DB_PREFIX  --dbname=$DB_NAME --dbuser=$DB_USER --dbpass=$DB_PASS
+	wp core install --url=$WP_URL --title="$WP_TITLE" --admin_name=$WP_ADMIN --admin_password=$WP_PASS --admin_email=$WP_MAIL
+	wp user create ann ann@example.com
 fi
 
 php-fpm7.4 -F
