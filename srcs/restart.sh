61a3e8261c380ece0214e7d0e9d0fdda6c75c770
shalimi <Shai.halimi@gmail.com>
2023-08-23 16:43:46 +0200

    rm restart

--- srcs/restart.sh
+++
@@ -1,6 +0,0 @@
-docker stop $(docker ps -qa)
-docker rm $(docker ps -qa)
-docker rmi -f $(docker images -qa)
-docker volume rm $(docker volume ls -q)
-docker network rm $(docker network ls -q)
-docker system prune -f
