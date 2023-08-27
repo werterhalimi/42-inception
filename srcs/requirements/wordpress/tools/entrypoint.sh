a9a113fa5bc64680875c6fd1a976e55fceb7badd
Shai Halimi <shai.halimi@gmail.com>
2023-08-27 23:59:12 +0200

    fixed some erros

--- srcs/requirements/wordpress/tools/entrypoint.sh
+++ srcs/requirements/wordpress/tools/entrypoint.sh
@@ -1,5 +1,6 @@
-if ! ([ -f "/var/www/wp-config.php" ];) then
+if ! ([ -f "/var/www/wordpress/wp-config.php" ];) then
 	wp core download
+	echo $WP_URL
 	wp config create --dbhost=$DB_HOST --dbprefix=$DB_PREFIX  --dbname=$DB_NAME --dbuser=$DB_USER --dbpass=$DB_PASS
 	wp core install --url=$WP_URL --title="$WP_TITLE" --admin_name=$WP_ADMIN --admin_password=$WP_PASS --admin_email=$WP_MAIL
 	wp user create ann ann@example.com
@@ -10,4 +11,4 @@ if ! ([ -f "/var/www/wp-config.php" ];) then
 	wp redis enable
 fi
 
-php-fpm7.4 -F
+php-fpm8.2 -F
